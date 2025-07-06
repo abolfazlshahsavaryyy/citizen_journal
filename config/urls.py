ffa26d3a392eb754cec14f44a2d8c7ea1e480537
Abolfazl <abolfazlshahsavaryyy@gmail.com>
2025-07-06 17:59:53 +0330

    fix: typo capitalize name

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pages/', include('page.urls')),
+    path('pages/', include('Page.urls')),
 ]
