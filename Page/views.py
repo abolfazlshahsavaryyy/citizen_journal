eebdcd2310c0e5ea4a99f4e18cd22109ad917c4e
Abolfazl <abolfazlshahsavaryyy@gmail.com>
2025-07-06 13:23:32 +0330

    feat: add generic view for page

--- Page/views.py
+++ Page/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+# myapp/views.py
 
-# Create your views here.
+from rest_framework import generics
+from .models import Page
+from .serelizers import PageSerializer
+
+class PageListCreateView(generics.ListCreateAPIView):
+    """
+    GET: List all pages
+    POST: Create a new page
+    """
+    queryset = Page.objects.all().order_by('-follower_count')
+    serializer_class = PageSerializer
+
+
+class PageDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    GET: Retrieve a specific page by ID
+    PUT: Update a page (full update)
+    DELETE: Delete a page
+    """
+    queryset = Page.objects.all()
+    serializer_class = PageSerializer
