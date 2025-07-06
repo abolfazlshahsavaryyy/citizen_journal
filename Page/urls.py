6878326b059a14139f50d91953c17421ebb990be
Abolfazl <abolfazlshahsavaryyy@gmail.com>
2025-07-06 18:00:41 +0330

    fix: comment the detail url

--- Page/urls.py
+++ Page/urls.py
@@ -1,9 +1,9 @@
 # myapp/urls.py
 
 from django.urls import path
-from .views import PageListCreateView, PageDetailView
+from .views import PageListCreateView
 
 urlpatterns = [
     path('pages/', PageListCreateView.as_view(), name='page-list-create'),
-    path('pages/<int:pk>/', PageDetailView.as_view(), name='page-detail'),
+    #path('pages/<int:pk>/', PageDetailView.as_view(), name='page-detail'),
 ]
