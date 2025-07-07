ab7879ec4999e574ed2c48d23d9db83b7bcbbb3e
Abolfazl <abolfazlshahsavaryyy@gmail.com>
2025-07-07 12:18:54 +0330

    feat: add the url of news to page urls file for page app

--- Page/urls.py
+++ Page/urls.py
@@ -2,11 +2,14 @@
 
 from django.urls import path
 from Page.views.page_views import *
-from Page.views.follow_views import FollowDetailesView,FollowListView
+from Page.views.follow_views import *
+from Page.views.news_views import *
 
 urlpatterns = [
     path('pages/', PageListCreateView.as_view(), name='page-list-create'),
     path('pages/<int:pk>/', PageDetailView.as_view(), name='page-detail'),
     path('follow/',FollowListView.as_view(),name='follow-create'),
-    path('follow/<int:pk>/', FollowDetailesView.as_view(),name='follow-details')
+    path('follow/<int:pk>/', FollowDetailesView.as_view(),name='follow-details'),
+    path('news/', NewsListCreateView.as_view(), name='news-list-create'),
+    path('news/<int:pk>/', NewsDetailView.as_view(), name='news-detail')
 ]
