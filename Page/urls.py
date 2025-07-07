8f44498ff24af8a4c96df895a1a4709eed4998a2
Abolfazl <abolfazlshahsavaryyy@gmail.com>
2025-07-07 09:32:22 +0330

    feat; add the url for follow endpoints

--- Page/urls.py
+++ Page/urls.py
@@ -2,8 +2,11 @@
 
 from django.urls import path
 from Page.views.page_views import *
+from Page.views.follow_views import FollowDetailesView,FollowListView
 
 urlpatterns = [
     path('pages/', PageListCreateView.as_view(), name='page-list-create'),
     path('pages/<int:pk>/', PageDetailView.as_view(), name='page-detail'),
+    path('follow/',FollowListView.as_view(),name='follow-create'),
+    path('follow/<int:pk>/', FollowDetailesView.as_view(),name='follow-details')
 ]
