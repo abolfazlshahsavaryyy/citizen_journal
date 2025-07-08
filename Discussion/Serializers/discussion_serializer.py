61f1f18be011f938c826a69fb03881fb9dd0be64
Abolfazl <abolfazlshahsavaryyy@gmail.com>
2025-07-08 13:30:09 +0330

    feat: add serializer and views file for lated develop but the Discussion serilizer is complete

---
+++ Discussion/Serializers/discussion_serializer.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+from Discussion.models import Discussion, Topic
+
+
+class DiscussionListSerializer(serializers.ModelSerializer):
+    short_description = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Discussion
+        fields = ['id', 'name', 'short_description', 'is_active', 'created_at']
+
+    def get_short_description(self, obj):
+        return obj.description[:100]  # Return first 100 characters
+
+
+class TopicTitleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Topic
+        fields = ['title']
+
+
+class DiscussionDetailSerializer(serializers.ModelSerializer):
+    topics = TopicTitleSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Discussion
+        fields = [
+            'id', 'name', 'description', 'is_active',
+            'created_at', 'updated_at', 'topics'
+        ]
+
+
+class DiscussionCreateUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Discussion
+        fields = ['name', 'description', 'is_active']
