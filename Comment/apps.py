d76f41d50319dffaea9abdfb7814755a4bdff92a
Abolfazl <abolfazlshahsavaryyy@gmail.com>
2025-07-08 09:10:44 +0330

    feat: add singnal decoratore to change comment count while comment added or removerd

--- Comment/apps.py
+++ Comment/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
-
 class CommentConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Comment'
+
+    def ready(self):
+        import Comment.signal.signals
