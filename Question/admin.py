72fbc100f764d43cd1de47290b1006b14588a9e1
Abolfazl <abolfazlshahsavaryyy@gmail.com>
2025-07-09 20:30:43 +0330

    feat: add Question and Answer to admin and create Answer table using migrations

--- Question/admin.py
+++ Question/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from Question.models.question import Question
+from Question.models.answer import Answer
 # Register your models here.
+admin.site.register(Question)
+admin.site.register(Answer)
