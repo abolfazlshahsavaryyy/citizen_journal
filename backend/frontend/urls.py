6bb0a0fea1ec5cfb0f25cf71827b389ad44841c9
Abolfazl <abolfazlshahsavaryyy@gmail.com>
2025-07-23 20:49:34 +0330

    feat: add front for news details and prediction

--- backend/frontend/urls.py
+++ backend/frontend/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('dashboard/', views.dashboard_view, name='dashboard'),
     path('logout/', views.logout_view, name='logout'),
     path('page/<int:page_id>/', views.page_detail_view, name='page_detail'), 
+    path('pages/news/<int:news_id>/', views.news_detail_view, name='news_detail'),
+    path('pages/news/<int:news_id>/predict/', views.predict_news_view, name='predict_news'),
 ]
