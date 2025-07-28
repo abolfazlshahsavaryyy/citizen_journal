f6bfa3f240ce80bfa0ae6b0c362556782da8da0a
Abolfazl <abolfazlshahsavaryyy@gmail.com>
2025-07-28 13:39:03 +0330

    refactore: change the dicussion url pattern

--- backend/django_backend/Discussion/urls.py
+++ backend/django_backend/Discussion/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('discussions/<int:pk>/', DiscussionDetailView.as_view(), name='discussion-detail'),
     path('topics/', TopicListCreateView.as_view(), name='topic-list-create'),
     path('topics/<int:pk>/', TopicDetailView.as_view(), name='topic-detail'),
-    path('discussions/create/<int:page_pk>', DiscussionCreateForPageView.as_view(), name='discussion-create-for-page'),
+    path('discussions/create/<int:page_pk>/', DiscussionCreateForPageView.as_view(), name='discussion-create-for-page'),
     path('discussions/<int:discussion_id>/topics/', GetTopicOfDiscussion.as_view(), name='discussion-topics'),
 
 ]
