3d83f48704434ba4f8a969f7759c514f34a6baa7
Abolfazl <abolfazlshahsavaryyy@gmail.com>
2025-08-07 15:09:09 +0330

    feat: configure the task create after each like

--- backend/django_backend/Page/services/like_service.py
+++ backend/django_backend/Page/services/like_service.py
@@ -1,26 +1,26 @@
 # services.py
 from django.db import transaction
-
+from Notification.tasks import *
+#from Page.task import send_notification
 def toggle_news_like(news, user):
-    """
-    Toggles the like status of a news post for a user.
-    Returns:
-      (action: str, message: str)
-      action = 'liked' or 'unliked'
-    """
     with transaction.atomic():
         if news.likes.filter(id=user.id).exists():
-            # User already liked → remove
             news.likes.remove(user)
             action = "unliked"
             message = "Unliked successfully."
         else:
-            # User has not liked → add
             news.likes.add(user)
             action = "liked"
             message = "Liked successfully."
 
-        # Update like_count
+            # Send async notification to the owner of the news post
+            if news.page.user != user:
+                send_notification.delay(
+                    user_id=news.page.user.id,
+                    message=f"{user.username} liked your news post.",
+                    data={"news_id": news.id}
+                )
+
         news.like_count = news.likes.count()
         news.save(update_fields=["like_count"])
 
