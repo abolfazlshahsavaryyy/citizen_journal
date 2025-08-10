ba7fdded790d85b8cf7e45411f76c975c1623648
Abolfazl <abolfazlshahsavaryyy@gmail.com>
2025-08-10 12:43:36 +0330

    feat: configure setting changes on the manage.py and celery file

--- backend/django_backend/config/celery.py
+++ backend/django_backend/config/celery.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')  # Adjust if needed
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')  # Adjust if needed
 
 app = Celery('config')
 app.config_from_object('django.conf:settings', namespace='CELERY')
