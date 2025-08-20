f739dec333b8943655d94a31980d5e4c7a70fc6b
Abolfazl <abolfazlshahsavaryyy@gmail.com>
2025-08-20 23:26:37 +0330

    feat: add recommandation api for news that users has been like that are in same user activity based on likes

--- backend/django_backend/Page/graphql/schema.py
+++ backend/django_backend/Page/graphql/schema.py
@@ -2,6 +2,8 @@ import graphene
 from graphene_django.types import DjangoObjectType
 from Page.models.News import News
 import graphql_jwt
+from django.contrib.auth import get_user_model
+
 
 class NewsType(DjangoObjectType):
     class Meta:
@@ -10,12 +12,38 @@ class NewsType(DjangoObjectType):
 
 class Query(graphene.ObjectType):
     all_news = graphene.List(NewsType)
+    recommended_news = graphene.List(NewsType)
 
     def resolve_all_news(self, info):
         user = info.context.user
         if user.is_anonymous:
             raise Exception("Authentication required!")
         return News.objects.all()
+
+    def resolve_recommended_news(self, info):
+        user = info.context.user
+        if user.is_anonymous:
+            raise Exception("Authentication required!")
+
+        # 1. News that current user likes
+        user_liked_news = News.objects.filter(likes=user)
+
+        # 2. Other users who liked those news
+        similar_users = (
+            get_user_model()
+            .objects.filter(liked_news__in=user_liked_news)
+            .exclude(id=user.id)
+            .distinct()
+        )
+
+        # 3. News those users like
+        recommended_news = (
+            News.objects.filter(likes__in=similar_users)
+            .exclude(likes=user)  # remove ones user already likes
+            .distinct()
+        )
+
+        return recommended_news
 
 class Mutation(graphene.ObjectType):
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
