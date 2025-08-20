626b53845433dadcb1ef2881da32cb56548e6119
Abolfazl <abolfazlshahsavaryyy@gmail.com>
2025-08-20 10:02:24 +0330

    feat: add schema for grapgql for get all news and authentication using graphql

--- backend/django_backend/Page/graphql/schema.py
+++ backend/django_backend/Page/graphql/schema.py
@@ -1,5 +1,26 @@
-# app/graphql/schema.py
-import strawberry
-from Page.graphql.queries import Query
+import graphene
+from graphene_django.types import DjangoObjectType
+from Page.models.News import News
+import graphql_jwt
 
-schema = strawberry.Schema(query=Query)
+class NewsType(DjangoObjectType):
+    class Meta:
+        model = News
+
+
+class Query(graphene.ObjectType):
+    all_news = graphene.List(NewsType)
+
+    def resolve_all_news(self, info):
+        user = info.context.user
+        if user.is_anonymous:
+            raise Exception("Authentication required!")
+        return News.objects.all()
+
+class Mutation(graphene.ObjectType):
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
+
