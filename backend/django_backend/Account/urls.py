379647ce4f1fe73bade5db9021142169cf0fb7db
Abolfazl <abolfazlshahsavaryyy@gmail.com>
2025-10-01 14:35:53 +0330

    feat: configure thte url for this api

--- backend/django_backend/Account/urls.py
+++ backend/django_backend/Account/urls.py
@@ -3,7 +3,11 @@
 from django.urls import path
 from Account.views.profile_view import ProfileDetailView
 from Account.views.user_view import RegisterView
+from Account.views.public_key import PublicKeyView
 urlpatterns = [
     path('me/', ProfileDetailView.as_view(), name='profile-detail'),
     path('register/', RegisterView.as_view(), name='register'),
+    path("api/public-key/", PublicKeyView.as_view(), name="public_key"),
 ]
+
+
