1f2ea66e70329d61779d1f1dc54076c25df19eaa
Abolfazl <abolfazlshahsavaryyy@gmail.com>
2025-10-02 15:26:00 +0330

    feat: configure authentication mediatR and Carter and ef core for sqlite configuration

--- backend/AspNetCoreServices/SaveNews/Program.cs
+++ backend/AspNetCoreServices/SaveNews/Program.cs
@@ -1,6 +1,69 @@
+using Microsoft.EntityFrameworkCore;
+using SaveNews.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Net.Http;
+using System.Security.Cryptography;
+using SaveNews.Handler.CreateSaveNews; 
+using Carter;
+using MediatR;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+//configure the ef core for sqlite
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//configure the http route to Django app
+builder.Services.AddHttpClient("PublicKeyClient", client =>
+{
+    client.BaseAddress = new Uri("http://localhost:8000/");
+});
+//configure mediatR for CQRS
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(SaveNewsCommandHandler).Assembly));
+
+
+// Fetch the public key from Django
+using var httpClient = new HttpClient();
+var publicKeyText = await httpClient.GetStringAsync("http://localhost:8000/account/api/public-key/");
+
+// Import into RSA
+using var rsa = RSA.Create();
+rsa.ImportFromPem(publicKeyText.ToCharArray());
+
+var key = new RsaSecurityKey(rsa);
+
+// Configure JWT authentication
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        RequireExpirationTime = true,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = key,
+        ClockSkew = TimeSpan.Zero
+    };
+});
+
+builder.Services.AddAuthorization();
+builder.Services.AddCarter();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapCarter();
+
 
 app.Run();
